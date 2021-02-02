32576c6ef1868e94e6423f922d0e18d0edc7b80e
Pranil Kakade <pranilkakade111@gmail.com>
2021-02-02 23:38:56 +0530

    Display Calculation Between Two Point

---
+++ LineComputation/src/LineCompute.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LineCompute 
+{
+
+	public static void main(String[] args) 
+	{
+	 System.out.println("Calculate Distance Between Two Point");		 
+	}
+		 
+}
+
+
